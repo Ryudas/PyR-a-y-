62c7b5553fe3dff732f8c7cbcfbdfb203793575b
localadmin <sivlesegrob9@gmail.com>
2019-08-10 00:34:19 +0200

    Restructured predefined variables

--- raytracer.py
+++ raytracer.py
@@ -2,8 +2,16 @@ import pygame as pg
 import numpy as np
 import random
 
+
+HEIGHT = 320
+WIDTH = 480
+# set the size of the screen as multiples of the array
+cellsize = 1
+SCREEN_HEIGHT = HEIGHT * cellsize 
+SCREEN_WIDTH = WIDTH * cellsize
+
 # create a 3D array with 600x600x3 (the last dimension is for the RGB color)
-cells = np.ndarray((480, 320, 3)) 
+pixel_array = np.ndarray((HEIGHT, WIDTH, 3)) 
 
 # color dictionary, represents white, red and blue
 color_dict = {
@@ -13,31 +21,34 @@ color_dict = {
         }
 
 # pick a random color tuple from the color dict
-for i in range(cells.shape[0]):
-    for j in range(cells.shape[1]):
-        cells[i][j] = color_dict[random.randrange(3)]
+for i in range(HEIGHT):
+    for j in range(WIDTH):
+        pixel_array[i][j] = color_dict[random.randrange(3)]
 
-# set the size of the screen as multiples of the array
-cellsize = 1
-WIDTH = cells.shape[0] * cellsize 
-HEIGHT = cells.shape[1] * cellsize
 
-# initialize pygame
+
+# Initialize pygame
 pg.init()
+
+# Initialize the screen for display
 screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+# Clock for limiting framerate of window
 clock = pg.time.Clock()
 
-#create a surface with the size as the array 
-surf = pg.Surface((cells.shape[0], cells.shape[1]))
+
+# Create a surface with the size as the array 
+surf = pg.Surface((WIDTH, HEIGHT))
+
  # draw the array onto the surface 
-pg.surfarray.blit_array(surf, cells)
+pg.surfarray.blit_array(surf, pixel_array)
 # transform the surface to screen size
 surf = pg.transform.scale(surf, (WIDTH, HEIGHT)) 
 
 # game loop
 running, i = True, -1
-print(cells.shape[0] )
-print(cells.shape[1] )
+print(pixel_array.shape[0] ) 
+print(pixel_array.shape[1] )
  
 while running: 
     #clock.tick(3000)
@@ -48,16 +59,16 @@ while running:
     print(i) 
  
 
-    for event in pg.event.get(): 
+    for event in pg.event.get():  
         if event.type == pg.QUIT:
             running = False
 
     # for all         
-    for k in range(cells.shape[0]):
-        cells[k][i] = color_dict[random.randrange(3)]
+    for k in range(pixel_array.shape[0]):
+        pixel_array[k][i] = color_dict[random.randrange(3)]
 
     # update screen with pixel array        
-    pg.surfarray.blit_array(surf, cells)    
+    pg.surfarray.blit_array(surf, pixel_array)    
     screen.fill((0, 0, 0))
 
     # blit the transformed surface onto the screen
